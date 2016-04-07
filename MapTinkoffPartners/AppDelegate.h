5a6a69c20e1e4155ab79f9dad7d6fc77544d3970
alexander <fairk19@gmail.com>
2016-04-07 11:40:55 +0300

    Initial Commit

---
+++ MapTinkoffPartners/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapTinkoffPartners
+//
+//  Created by Александр on 07.04.16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapTinkoffPartners/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MapTinkoffPartners
+//
+//  Created by Александр on 07.04.16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
