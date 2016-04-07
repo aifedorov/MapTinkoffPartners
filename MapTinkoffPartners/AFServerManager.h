445d84d492b7efacf6ac4f3920255b438747e85a
alexander <fairk19@gmail.com>
2016-04-07 11:50:15 +0300

    Добавлен класс AFServerManager для работы с сетью Добавлен метод для создания объекта Singleton

---
+++ MapTinkoffPartners/AFServerManager.h
@@ -0,0 +1,15 @@
+//
+//  AFServerManager.h
+//  MapTinkoffPartners
+//
+//  Created by Александр on 07.04.16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFServerManager : NSObject
+
++ (instancetype) sharedManager;
+
+@end
