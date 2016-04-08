15c1ab924d4897380983342099dc01bae9de28f0
alexander <fairk19@gmail.com>
2016-04-08 11:56:42 +0300

    Добавлены модели Partner и  DepositionPoint

--- MapTinkoffPartners/AppDelegate.m
+++ MapTinkoffPartners/AppDelegate.m
@@ -28,6 +28,13 @@
 //    
 //    [manager fetchPartners:requestString];
     
+//    
+//    NSManagedObject *partner = [NSEntityDescription insertNewObjectForEntityForName:@"Partner" inManagedObjectContext:self.managedObjectContext];
+//    
+//    [partner setValue:@"MTS" forKey:@"idPartner"];
+//    
+//    [self saveContext];
+    
     return YES;
 }
 
@@ -69,7 +76,7 @@
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"test" withExtension:@"momd"];
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"MapTinkoffPartners" withExtension:@"momd"];
     _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     return _managedObjectModel;
 }
@@ -83,7 +90,7 @@
     // Create the coordinator and store
     
     _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"test.sqlite"];
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"MapTinkoffPartners.sqlite"];
     NSError *error = nil;
     NSString *failureReason = @"There was an error creating or loading the application's saved data.";
     if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
